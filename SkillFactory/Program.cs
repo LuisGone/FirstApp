052455e70c0213bd260b446b2888bc3fc81fc1b8
Nikita Deriabin <luis.one@bk.ru>
2023-12-11 18:49:41 +0400

    Ура, я инициализировал переменную

--- SkillFactory/Program.cs
+++ SkillFactory/Program.cs
@@ -10,6 +10,9 @@ namespace SkillFactory
     {
         static void Main(string[] args)
         {
+            string MyName;
+            MyName = "Jane";
+            Console.WriteLine(MyName);
         }
     }
 }
